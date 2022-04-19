2a854e9f7258d440e1108e47fff6ba89e389c273
rootdownenergy <99987396+rootdownenergy@users.noreply.github.com>
2022-04-18 18:40:34 -0600

    Update NeoApiService.kt

--- app/src/main/java/com/rootdown/dev/nasaneorebase/data/net/NeoApiService.kt
+++ app/src/main/java/com/rootdown/dev/nasaneorebase/data/net/NeoApiService.kt
@@ -5,6 +5,6 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface NeoApiService {
-    @GET("neo/rest/v1/feed/today?detailed=true&api_key=cUsQuzCnvyTyHhyuaLJFLUjs9jRGJmAoogD9zWYH")
+    @GET("neo/rest/v1/feed/today?detailed=true&api_key=DEMO_KEY")
     suspend fun getNeo(): Response<NeoFeed>
 }
